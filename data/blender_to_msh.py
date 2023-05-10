239676cbe7e7ac137247fc3eefc43dc4f2db1b0a
bhiaibogf <bhiaibogf@outlook.com>
2023-05-10 15:59:00 +0800

    fix fov

--- data/blender_to_msh.py
+++ data/blender_to_msh.py
@@ -25,26 +25,29 @@ def main():
     path = json_dic['frames'][0]['file_path']
     img = cv2.imread(path + '.png', -1)
     print(img.shape)
-    w = img.shape[1]
+    h, w = img.shape[0], img.shape[1]
 
     dir = os.path.dirname(path)
     if not os.path.exists('data'):
         shutil.copy(dir, 'data')
 
     f_x = json_dic['fl_x']
-    fov = 2 * math.atan(w / 2 / f_x)
-    print(math.degrees(fov))
+    fov_x = math.degrees(2 * math.atan(w / 2 / f_x))
+    f_y = json_dic['fl_y']
+    fov_y = math.degrees(2 * math.atan(h / 2 / f_y))
+    print(fov_x, fov_y)
 
     pos_list = []
     tar_list = []
     rotation_list = []
 
-    for frame in json_dic['frames']:
+    for i, frame in enumerate(json_dic['frames']):
         pose = np.array(frame['transform_matrix'])
         # pose[:3, :3] = pose[:3, :3] @ np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
         pos = convert_inv(pose[:3, 3])
         dir = -convert_inv(pose[:3, 2])
         target = pos + dir
+        print(i, pos, target)
 
         pos_list.append(pos)
         tar_list.append(target)
@@ -53,7 +56,7 @@ def main():
     np.save('in_camOrgs.npy', np.stack(pos_list))
     np.save('in_camAts.npy', np.stack(tar_list))
     np.save('in_camExtrinsics.npy', np.stack(rotation_list))
-    np.save('fov.npy', fov)
+    np.save('fov.npy', fov_x)
 
 
 if __name__ == '__main__':
